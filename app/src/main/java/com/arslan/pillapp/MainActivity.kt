88f37840562d2cfbcc7046ec85655752ebcd6870
Arslan Sarıkaya <sarikayarslan@gmail.com>
2022-06-01 13:02:48 +0100

    Creating pill SQLTable

--- app/src/main/java/com/arslan/pillapp/MainActivity.kt
+++ app/src/main/java/com/arslan/pillapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.arslan.pillapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        try{
+            val database =this.openOrCreateDatabase("pill", Context.MODE_PRIVATE,null)
+            database.execSQL("CREATE TABLE IF NOT EXISTS pill(id INTEGER PRIMARY KEY,name VARCHAR)")
+
+
+
+        }catch (e : Exception){
+            e.printStackTrace()
+        }
+
     }
 }
